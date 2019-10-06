29ea093ba6f1d1cadec32698dcac396190243c34
RhAnjiE <irhanjie@gmail.com>
2019-10-07 01:54:12 +0200

    Improve tank movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,33 +9,31 @@ using Vector4 = UnityEngine.Vector4;
 
 public class PlayerMovement : MonoBehaviour {
     public Transform camera;
-    public float acceleration = 40f;
-    public float rotationSpeed = 20f;
+    public float acceleration = 5f;
+    public float rotationSpeed = 30f;
     
     private Transform transform;
-    private Rigidbody2D rigidbody;
 
     private float velocity;
     private float rotationVelocity;
 
     private void Start() {
-        rigidbody = GetComponent<Rigidbody2D>();
         transform = GetComponent<Transform>();
     }
     
     private void Update() {
         velocity = Input.GetAxis("Vertical") * acceleration;
         
-        rotationVelocity = -Input.GetAxis("Horizontal");
+        rotationVelocity = Input.GetAxis("Horizontal") * rotationSpeed;
+        
+        if (velocity >= 0f)
+            rotationVelocity *= -1;
     }
 
     private void FixedUpdate() {
-        rigidbody.AddRelativeForce(Vector3.up * velocity);
-        rigidbody.AddTorque(rotationVelocity * rotationSpeed);
-
+        transform.Translate(Vector3.up * (velocity * Time.fixedDeltaTime));
+        transform.Rotate(0f, 0f, rotationVelocity * Time.fixedDeltaTime);
+        
         camera.position = new Vector3(transform.position.x, transform.position.y, -10);
-
-        //rigidbody.rotation += rotationVelocity;
-        //player.Translate(Vector3.up * (velocity * Time.deltaTime));
     }
 }
