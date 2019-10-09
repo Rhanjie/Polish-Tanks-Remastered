4c09eee819a86bd670d0ff14f18dc39dee857f76
RhAnjiE <irhanjie@gmail.com>
2019-10-09 22:40:37 +0200

    Add turret rotating

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,31 +9,51 @@ using Vector4 = UnityEngine.Vector4;
 
 public class PlayerMovement : MonoBehaviour {
     public Transform camera;
+    public Transform turret;
     public float acceleration = 5f;
     public float rotationSpeed = 30f;
+    public double turretRotationSpeed = 15f;
     
-    private Transform transform;
+    private Transform body;
 
     private float velocity;
     private float rotationVelocity;
+    private double newRotation;
+    private int direction;
 
     private void Start() {
-        transform = GetComponent<Transform>();
+        body = GetComponent<Transform>();
     }
     
     private void Update() {
         velocity = Input.GetAxis("Vertical") * acceleration;
-        
         rotationVelocity = Input.GetAxis("Horizontal") * rotationSpeed;
-        
+
         if (velocity >= 0f)
             rotationVelocity *= -1;
+
+        Vector3 convertedMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        newRotation = Mathf.Rad2Deg * Math.Atan2(
+            convertedMousePosition.y - turret.position.y,
+            convertedMousePosition.x - turret.position.x);
+        
+        //if (turret.eulerAngles.z - currentRotation < 180 && turret.eulerAngles.z - currentRotation > -180)
+        if (newRotation < 0)
+            direction = -1;
+
+        else direction = 1;
     }
 
     private void FixedUpdate() {
-        transform.Translate(Vector3.up * (velocity * Time.fixedDeltaTime));
-        transform.Rotate(0f, 0f, rotationVelocity * Time.fixedDeltaTime);
+        body.Translate(Vector3.up * (velocity * Time.fixedDeltaTime));
+        body.Rotate(0f, 0f, rotationVelocity * Time.fixedDeltaTime);
         
-        camera.position = new Vector3(transform.position.x, transform.position.y, -10);
+        if ((int) turret.eulerAngles.z != (int) newRotation) {
+            turret.Rotate(new Vector3(0f, 0f, (float) turretRotationSpeed * direction * Time.fixedDeltaTime));
+        }
+
+        Vector2 position = body.position;
+        camera.position = new Vector3(position.x, position.y, -10);
     }
 }
