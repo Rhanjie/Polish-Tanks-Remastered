3d16d0ce383a0758e4fc9e9127ebb218141af923
RhAnjiE <irhanjie@gmail.com>
2019-10-12 13:11:29 +0200

    Extend shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class Bullet : MonoBehaviour {
+    public Transform shooter;
+    
+    private void Update() {
+        if (Vector2.Distance(shooter.position, transform.position) > 30)
+            Destroy(gameObject);
+        
+        Debug.Log(Vector2.Distance(shooter.position, transform.position));
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        Destroy(gameObject);
+        
+        //TODO: Play explosion and take damage
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+    public Transform shootPlace;
+    public Rigidbody2D bulletPrefab;
+    public ParticleSystem shootFogPrefab;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+    }
+    
+    private void Shoot() {
+        Rigidbody2D bullet = Instantiate(bulletPrefab, shootPlace.position, shootPlace.rotation);
+        bullet.AddRelativeForce(-Vector2.left * 30f, ForceMode2D.Impulse);
+        bullet.GetComponent<Bullet>().shooter = transform;
+
+        Instantiate(shootFogPrefab, shootPlace.position, shootPlace.rotation);
+    }
+}
