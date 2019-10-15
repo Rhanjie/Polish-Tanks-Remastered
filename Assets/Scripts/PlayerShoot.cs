ec163338622eab4ac0749ce836967f17cd826168
RhAnjiE <irhanjie@gmail.com>
2019-10-15 13:10:25 +0200

    Add reload time

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,26 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerShoot : MonoBehaviour {
     public Transform shootPlace;
     public Rigidbody2D bulletPrefab;
     public ParticleSystem shootFogPrefab;
+    public Text reloadText;
+
+    public float reloadTime = 2f;
 
     private Rigidbody2D body;
+    private float currentReloadTime;
 
     private void Start() {
         body = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
     private void Update() {
-        if (Input.GetButtonDown("Fire1")) {
+        if (currentReloadTime < reloadTime)
+            currentReloadTime += Time.deltaTime;
+
+        if (currentReloadTime > reloadTime)
+            currentReloadTime = reloadTime;
+
+        //TODO: Temporary way, change it soon
+        reloadText.text = "Reload: " + currentReloadTime.ToString("0.00") + "/" + reloadTime + " sec";
+        
+        if (Input.GetButtonDown("Fire1") && currentReloadTime >= reloadTime) {
             Shoot();
         }
     }
     
     private void Shoot() {
+        currentReloadTime = 0f;
+        
         Rigidbody2D bullet = Instantiate(bulletPrefab, shootPlace.position, shootPlace.rotation);
         bullet.AddRelativeForce(-Vector2.left * 30f, ForceMode2D.Impulse);
         bullet.GetComponent<Bullet>().shooter = transform;
